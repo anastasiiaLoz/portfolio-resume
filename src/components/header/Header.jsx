4322a8871a51c4e7ffd74006110a658a5311da2f
Anastasiia Lozovych <anna.collana@gmail.com>
2023-05-29 19:53:10 +0100

    Setting up some changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Anastasiia Lozovych</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">MERN Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
